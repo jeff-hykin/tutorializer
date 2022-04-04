2544cf81f49b6d7acac5f89f71a7f38ebc6f1d8f
Jeff Hykin <jeff.hykin@gmail.com>
2022-04-04 08:44:17 -0500

    -

--- website.jsx
+++ website.jsx
@@ -1,16 +1,37 @@
-import quikRouter from 'quik-router'
-// import quikRouter from 'https://cdn.skypack.dev/quik-router'
+;((async ()=>{
+    const { default: router } = await import('https://cdn.skypack.dev/quik-router')
+    window.router = router // debugging
+    
+    if (router.pageInfo.url) {
+        try {
+            var {onLoad} = await import(router.pageInfo.url)
+        } catch (error) {
 
-document.head.innerHTML += `<link rel="stylesheet" href="https://unpkg.com/css-baseline/css/3.css">`
+        }
+    }
+    if (onLoad) {
+        console.log("running onload")
+        onLoad()
+    }
+    // router.pageInfo.page
+    // router.pageInfo.url = "https://cdn.jsdelivr.net/gh/jeff-hykin/tutorialize@master/virkshop/tutorialize.js"
+    
 
-document.body.style = `
-    display: flex; 
-    align-items: center; 
-    justify-content: center;
-    font-size: 45px; 
-    font-family: sans-serif;
-    height: 100%;
-`
-document.body.innerHTML = `
-    Hello World!
-`
+    document.head.innerHTML += `<link rel="stylesheet" href="https://unpkg.com/css-baseline/css/3.css">`
+
+    document.body.style = `
+        display: flex; 
+        align-items: center; 
+        justify-content: center;
+        font-size: 45px; 
+        font-family: sans-serif;
+        height: 100%;
+    `
+    document.body.innerHTML = `
+        Hello World!
+    `
+
+
+    
+    
+})())
