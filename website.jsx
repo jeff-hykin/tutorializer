285baf662c6267ea3c7fcced3f06d53fcb6b6a12
Jeff Hykin <jeff.hykin@gmail.com>
2022-04-04 08:20:18 -0500

    squashed entire history

---
+++ website.jsx
@@ -0,0 +1,11 @@
+document.body.style = `
+    display: flex; 
+    align-items: center; 
+    justify-content: center;
+    font-size: 45px; 
+    font-family: sans-serif;
+    height: 100%;
+`
+document.body.innerHTML = `
+    Hello World!
+`
